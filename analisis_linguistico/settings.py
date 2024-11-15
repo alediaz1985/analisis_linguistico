f648cef324bc46454c8b1c10a49b72c93e7514ee
Alejandro Leonel Díaz <88168944+alediaz1985@users.noreply.github.com>
2024-11-14 23:44:32 -0300

    actualizacion settings requeriment base

--- analisis_linguistico/settings.py
+++ analisis_linguistico/settings.py
@@ -104,13 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "America/Argentina/Buenos_Aires"
+USE_TZ = True
 
 USE_I18N = True
 
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
